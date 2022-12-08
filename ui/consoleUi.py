94f75b8cd35868c66d3a413923de21106436ad48
vizualjack <67346906+vizualjack@users.noreply.github.com>
2022-12-09 00:39:23 +0100

    consoleUi begin of editing series

--- ui/consoleUi.py
+++ ui/consoleUi.py
@@ -1,6 +1,8 @@
 from seriesData.library import Library
 from seriesData.series import Series
 from tracking.user import User
+import time
+import keyboard
 
 
 class ConsoleUI:
@@ -23,6 +25,8 @@ class ConsoleUI:
                         self.__createSeries()
                     elif option == "l":
                         self.__listSeries()
+                    elif option == "e":
+                        self.__editSeriesList()
             # elif option == "u":
             #     self.__printUserOptions()
             #     option = input()
@@ -37,8 +41,9 @@ class ConsoleUI:
     #     print("Nothing here")
 
     def __printLibraryOptions(self):
-        print("Create series - c")
         print("List series - l")
+        print("Create series - c")
+        print("Edit series - e")
 
 
     def __createSeries(self):
@@ -69,3 +74,35 @@ class ConsoleUI:
             return
         for series in self.library.series:
             print(series.name)
+
+
+    def __editSeriesList(self):
+        if len(self.library.series) == 0:
+            print("No series")
+            return
+        series = self.__selectSeries()
+        print(series.name + " selected")
+
+    
+    def __selectSeries(self):
+        seriesList = self.library.series
+        selectedIndex = 0
+        print("Use arrow and enter key to select series")
+        time.sleep(0.1)
+        while True:
+            selectedSeries = self.library.series[selectedIndex]
+            name = selectedSeries.name
+            name += " "*100
+            print(name, end='\r')
+            pressedKey = keyboard.read_key()
+            time.sleep(0.1)
+            if pressedKey == "enter":
+                return selectedSeries
+            elif pressedKey == "nach-oben":
+                selectedIndex -= 1
+            elif pressedKey == "nach-unten":
+                selectedIndex += 1
+            if selectedIndex >= len(seriesList):
+                selectedIndex = 0
+            elif selectedIndex < 0:
+                selectedIndex = len(seriesList)-1
