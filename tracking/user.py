18e8c40df4c2d0442b8f7b45f43bbd0490e8cc6e
vizualjack <67346906+vizualjack@users.noreply.github.com>
2022-12-09 22:29:38 +0100

    console ui added more options

--- tracking/user.py
+++ tracking/user.py
@@ -11,3 +11,13 @@ class User:
     
     def addSeries(self, series):
         self.watchInfos.append(WatchInfo(series))
+
+
+    def removeSeries(self, series):
+        watchInfoWithSeries = None
+        for watchInfo in self.watchInfos:
+            if watchInfo.series == series:
+                watchInfoWithSeries = watchInfo
+                break
+        if watchInfoWithSeries != None:
+            self.watchInfos.remove(watchInfoWithSeries)
--- ui/consoleUi.py
+++ ui/consoleUi.py
@@ -1,6 +1,7 @@
 from seriesData.library import Library
 from seriesData.series import Series
 from tracking.user import User
+from tracking.watchInfo import WatchInfo
 import time
 import keyboard
 
@@ -27,18 +28,46 @@ class ConsoleUI:
                         self.__listSeries()
                     elif option == "e":
                         self.__editSeriesList()
-            # elif option == "u":
-            #     self.__printUserOptions()
-            #     option = input()
+            elif option == "u":
+                while option != "":
+                    self.__printUserOptions()
+                    option = input()
+                    if option == "as":
+                        series = self.__selectSeries()
+                        input(series.name + " selected")
+                        confirm = input("Wanna add? y/n: ")
+                        print("confirm: " + confirm)
+                        if confirm == "y":
+                            self.user.addSeries(series)
+                            print("Added series to user")
+                    elif option == "rs":
+                        watchInfo = self.__selectWatchInfo()
+                        input()
+                        option = input("Type 'yes' for remove " + watchInfo.series.name + " from your list: ")
+                        if option == "yes":
+                            self.user.removeSeries(watchInfo.series)
+                            print("Removed series!")
+                    elif option == "ls":
+                        self.__printWatchInfos(self.user.watchInfos)
+
+
+    def __printWatchInfos(self, watchInfos:WatchInfo):
+        if len(watchInfos) == 0:
+            print("No series in your list")
+        for watchInfo in watchInfos:
+            print(watchInfo.series.name + " Season " + str(watchInfo.season) + " Episode " + str(watchInfo.episode))
+
+
+    def __printUserOptions(self):
+        print("Add series - as")
+        print("Remove series - rs")
+        print("List series - ls")
 
 
     def __printOptions(self):
-        # print("User - u")
+        print("User - u")
         print("Library - l")
 
-
-    # def __printUserOptions(self):
-    #     print("Nothing here")
 
     def __printLibraryOptions(self):
         print("List series - l")
@@ -81,7 +110,87 @@ class ConsoleUI:
             print("No series")
             return
         series = self.__selectSeries()
-        print(series.name + " selected")
+        print("Edit " + series.name)
+        self.__printEditOptions()
+        option = input("")
+        if option == "":
+            return
+        elif option == "r":
+            newName = input("New name: ")
+            if newName == "":
+                return
+            series.name = newName
+            print("Renamed!")
+        elif option == "cs":
+            self.__printSeasons(series)
+            while True:
+                season = input("Which season you wanna edit? ")
+                if season == "":
+                    break
+                seasonIndex = int(season)-1
+                if seasonIndex < 0 or seasonIndex >= len(series.seasons):
+                    continue
+                season = series.seasons[seasonIndex]
+                print("Edit season " + season)
+                self.__printSeasonInfo(season)
+                self.__printSeasonEditOptions()
+                option = input("")
+                if option == "n":
+                    newName = input("New name: ")
+                    if newName == "":
+                        return
+                    season.name = newName
+                    print("Name changed!")
+                elif option == "e":
+                    newEpisodes = input("New episodes: ")
+                    if newEpisodes == "":
+                        return
+                    newEpisodes = int(newEpisodes)
+                    season.episodes = newEpisodes
+                elif option == "delete":
+                    series.removeSeason(season)
+                    print("Season deleted!")
+        elif option == "as":
+            episodes = input("Episodes: ")
+            if episodes == "":
+                return
+            episodes = int(episodes)
+            if episodes <= 0:
+                return
+            seasonName = input("Season name(optional): ")
+            series.addSeason(episodes, seasonName)
+            print("Season added!")
+        elif option == "delete":
+            option = input("Type 'yes' for delete: ")
+            if option == "yes":
+                self.user.removeSeries(series)
+                self.library.removeSeries(series)
+                print("Series deleted!")
+
+    
+    def __printSeasonEditOptions(self):
+        print("Change name - n")
+        print("Change episodes - e")
+        print("Delete season - delete")
+
+
+    def __printSeasonInfo(self, season):
+        print("Name: " + season.name)
+        print("Episodes: " + season.episodes)
+
+    
+    def __printEditOptions(self):
+        print("Rename - r")
+        print("Change season - cs")
+        print("Add season - as")
+        print("Delete season - delete")
+
+
+    def __printSeasons(self, series:Series):
+        for seasonIndex in range(len(series.seasons)):
+            season = series.seasons[seasonIndex]
+            seasonNum = seasonIndex+1
+            print(str(seasonNum) + ". " + season.name + " | " + str(season.episodes) + " episodes")
 
     
     def __selectSeries(self):
@@ -106,3 +215,27 @@ class ConsoleUI:
                 selectedIndex = 0
             elif selectedIndex < 0:
                 selectedIndex = len(seriesList)-1
+
+
+    def __selectWatchInfo(self):
+        watchInfos = self.user.watchInfos
+        selectedIndex = 0
+        print("Use arrow and enter key to select series")
+        time.sleep(0.1)
+        while True:
+            selectedWatchInfo = self.user.watchInfos[selectedIndex]
+            name = selectedWatchInfo.series.name
+            name += " "*100
+            print(name, end='\r')
+            pressedKey = keyboard.read_key()
+            time.sleep(0.1)
+            if pressedKey == "enter":
+                return selectedWatchInfo
+            elif pressedKey == "nach-oben":
+                selectedIndex -= 1
+            elif pressedKey == "nach-unten":
+                selectedIndex += 1
+            if selectedIndex >= len(watchInfos):
+                selectedIndex = 0
+            elif selectedIndex < 0:
+                selectedIndex = len(watchInfos)-1
